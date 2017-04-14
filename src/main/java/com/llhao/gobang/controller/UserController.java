150f4bc1b9eeca25b5aed3d78181c60ae4c63d20
luohao07 <luohao07@meituan.com>
2017-04-14 19:09:02 +0800

    2017-04-14

--- src/main/java/com/llhao/gobang/controller/UserController.java
+++ src/main/java/com/llhao/gobang/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.llhao.gobang.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by HAO zong on 2017-4-13.
  */
 @Controller
 public class  UserController{
+    public UserController() {
+        System.out.println("Init.....");
+    }
 
+    public @ResponseBody String login(String name, String password){
+        return "Hello! "+name;
+    }
 }
