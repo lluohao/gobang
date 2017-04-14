2c42410cea876cab8c6c333aa5997fc8359823bd
luohao07 <luohao07@meituan.com>
2017-04-14 15:24:44 +0800

    改spring-context scope

--- src/main/java/com/llhao/gobang/controller/UserController.java
+++ src/main/java/com/llhao/gobang/controller/UserController.java
@@ -1,6 +1,11 @@
 package com.llhao.gobang.controller;
+
+import org.springframework.stereotype.Controller;
+
 /**
  * Created by HAO zong on 2017-4-13.
  */
+@Controller
 public class  UserController{
+
 }
